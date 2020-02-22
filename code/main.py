82e5614264090c6922a41c9b0065db70a05d4f11
Irin <irinbanerjee@gmail.com>
2020-02-22 12:28:46 +0530

    code to save results added

--- code/main.py
+++ code/main.py
@@ -16,5 +16,18 @@ import read
 column_names,x_train,y_train,train_count=read.read('../Data/1/train.csv')
 column_names,x_test,y_test,test_count=read.read('../Data/1/test.csv')
 
+#training- testing 
 import svm
-print (svm.SVM(x_train,y_train,x_test,y_test))
+accuracy,params=svm.SVM(x_train,y_train,x_test,y_test)
+
+#saving the result
+with open('Result/svm.csv',mode='a') as result_file:
+    result_writer=csv.writer(result_file)
+    fts=list()
+    for i in range(len(x_train[0])):
+        fts.append(1)
+    l=list()
+    l.aapend(fts)
+    l.append(params)
+    l.append(accuracy)
+    result_writer.writerow(l)
--- code/svm.py
+++ code/svm.py
@@ -12,5 +12,5 @@ def SVM(x_train,y_train,x_test,y_test):
     accuracy = accuracy_score(y_test,svm_predictions)
     print ("Accuracy: ",accuracy)
     print(clf.get_params)
-    return accuracy
+    return accuracy,clf.get_params
   
