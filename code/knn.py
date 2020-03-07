1d87e524028040311c16d0caa151062f035e4e95
sadrulmolla <mollasumon54@gmail.com>
2020-03-07 23:10:45 +0530

    adding knn'

---
+++ code/knn.py
@@ -0,0 +1,13 @@
+from sklearn.neighbors import KNeighborsClassifier
+def randomforest(x_train,y_train,x_test,y_test):
+	sc = StandardScaler()
+	x_train = sc.fit_transform(x_train)
+	x_test = sc.transform(y_test)
+	regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+	regressor.fit(x_train, y_train)
+	y_pred = regressor.predict(x_test)
+	accuracy = accuracy_score(y_test, y_pred)
+	print(confusion_matrix(y_test,y_pred))
+	print(classification_report(y_test,y_pred))
+	print(accuracy)
+	return accuracy
